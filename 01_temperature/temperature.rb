a64926c6164a642c5934746d4adc30d279b28999
Cory Parham <cory_parham24@hotmail.com>
2017-07-27 22:41:09 -0400

    Passed 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc temperatureF
+	temperatureC = temperatureF - 32
+	temperatureC = temperatureC * 5.0 / 9.0
+end
+
+def ctof temperatureC
+	temperatureF = temperatureC * 9.0 / 5.0
+	temperatureF = temperatureF + 32
+end

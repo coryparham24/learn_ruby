edc64bfd58ebca9ac170ad1fa5d0bcf4e8978b95
Cory Parham <cory_parham24@hotmail.com>
2017-07-28 23:34:28 -0400

    Passed 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,48 @@
 #write your code here
+def translate string
+	init = string
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	consonants = ['ch', 'qu', 'th', 'br']
+	threeConsonants = ['thr', 'sch', 'squ']
+	words = string.split(' ')
+
+	if words.length > 1
+		translated = []
+		words.each do |x|
+			newWord = translate(x)
+			translated = translated.push newWord
+		end
+		return string = translated.join(' ')
+	end
+
+	vowels.each do |x|
+		if x == string.downcase[0]
+			string = string + 'ay'
+		end
+	end
+
+	threeConsonants.each do |x|
+		if x == string.downcase[0] + string.downcase[1] + string.downcase[2]
+			letters = string.split('')
+			letters = letters.push letters.shift(3)
+			string = letters.join('') + 'ay'
+		end
+	end
+	
+	consonants.each do |x|
+		if x == string.downcase[0] + string.downcase[1]
+			letters = string.split('')
+			letters = letters.push letters.shift(2)
+			string = letters.join('') + 'ay'
+		end
+	end
+
+	if string == init
+		letters = string.split('')
+		letters = letters.push letters.shift
+		string = letters.join('') + 'ay'
+	end
+	
+	string
+
+end

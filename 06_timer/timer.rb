34ba368a2d47c17fb3c83de21b30e3d94b2ac5b3
Cory Parham <cory_parham24@hotmail.com>
2017-07-30 00:16:13 -0400

    Passed 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
-  #write your code here
+  
+	def seconds
+		@timer = 0
+	end
+
+	def seconds= time
+		@timer = time
+	end
+
+	def time_string
+		if @timer < 60
+			@timer = Time.new(1999, 1, 1, 0, 0, @timer)
+			@timer.strftime("%H:%M:%S").to_s
+		elsif @timer < 3600
+			minute = @timer / 60
+			second = @timer % 60
+			@timer = Time.new(1999, 1, 1, 0, minute, second)
+			@timer.strftime("%H:%M:%S").to_s
+		else
+			hour = @timer / 60 / 60
+			minute = (@timer / 60) % 60
+			second = @timer % ((60 * 60) + minute * 60)
+			@timer = Time.new(1999, 1, 1, hour, minute, second)
+			@timer.strftime("%H:%M:%S").to_s
+		end
+	end
+
+	def padded time
+		if time.to_s.length <2
+			time = '0' + time.to_s
+		else
+			time = time.to_s
+		end
+	end
+
 end
